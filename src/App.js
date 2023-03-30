dfbc6686952b0eae103e39b4505f4370f49b2986
antonio arista <ad.arista@acad.ucb.edu.bo>
2023-03-30 14:18:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import r1 from './images/r1.png';
+import r2 from './images/r2.png';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const refValue = useRef(0)
+
+  const increment = ( e ) => {
+
+    e.target.innerHTML = Number( e.target.innerHTML ) + 1
+
+ 
+    if(Number( e.target.innerHTML ) >= 10 ){
+      e.target.innerHTML = 1
+    }
+       Number( e.target.innerHTML ) >= 8
+    ?
+      e.target.style.backgroundColor="red"
+    :
+       e.target.style.backgroundColor="white"
+
+
+  }
+  const changeImg = (e) => {
+    e.target.src.includes(r1) 
+    ?
+      e.target.src = r2
+    :
+      e.target.src = r1
+    
+    
+  }
+  const product = () => {
+    refValue.current.innerHTML = Number(refValue.current.innerHTML) * 7
+  }
+  const onChangeText = ( e ) => {
+    
+    refValue.current.innerHTML = e.target.value
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div ref={ refValue } onClick={ increment } className="increment">1</div>
+    <button onClick={ product }>divisas</button>
+    <div><img onClick={ changeImg } src={ r1 }/></div>
+    <input onChange={ onChangeText }/>
+    </>
   );
 }
 
